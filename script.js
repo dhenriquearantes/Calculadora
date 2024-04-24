6c58862414b8d0f6f410deb6d8b5217504dc9cc6
Divino <noobonta@gmail.com>
2024-04-24 11:59:09 -0300

    Alterações na lógica e implementação de visual

--- script.js
+++ script.js
@@ -1,21 +1,55 @@
 var valorAcumulado = "";
+var state = 1;
+var valueOne = "";
+var valueTwo = "";
+var operador = "";
 var visor = document.getElementById('display');
 
+// var porcent = document.getElementById("porcent").value; 
+// console.log(porcent);
 
 function inputNum(valor) {
-  valorAcumulado += `${valor}`;
-  visor.setAttribute("value", valorAcumulado)
-  
+  valorAcumulado += valor;
+  document.getElementById('display').setAttribute("value", valorAcumulado);
 }
 
 document.getElementById("clear").addEventListener("click",() => {
   valorAcumulado = "";
-  visor.setAttribute("value", valorAcumulado)
-  
+  visor.setAttribute("value", valorAcumulado);
 })
 
 document.getElementById("del").addEventListener("click",() => {
-  valorAcumulado = valorAcumulado.substring(1)
-  console.log(valorAcumulado);
-
+  valorAcumulado = valorAcumulado.slice(0, -1);
+  visor.setAttribute("value", valorAcumulado)
 })
+
+function operator(valor) {
+  let resultado = 0;
+
+  if (valor == '+' || '-' || '*' || '/' || '%') {
+    operador = valor;
+    valueOne = valorAcumulado;
+    state = 2;
+  }  
+
+  switch (operador) {
+    case "+":
+      console.log("yesy");
+      break;
+    case "-":
+      resultado = Number(valorArray[0]) - Number(valorArray[2])
+      break
+    case "*":
+      resultado = Number(valorArray[0]) * Number(valorArray[2])
+      break     
+    case "/":
+      resultado = Number(valorArray[0]) / Number(valorArray[2])
+      break       
+    case "%":
+      resultado = 100 * Number(valorArray[0]) / Number(valorArray[2])
+      break            
+    default:
+      break;
+  }
+    
+}
